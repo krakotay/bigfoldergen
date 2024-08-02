4c3520129e7213c9f85db5f2e75f351f6b4b78e0
krakotay <krakotay@yandex.ru>
2024-08-02 13:13:29 +0300

    add Arguments

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ use std::path::{Path, PathBuf};
 
 type BigError = Box<dyn Error>;
 
-
 #[derive(Debug)]
 enum MyError {
     SizeParseError(String),
@@ -42,17 +41,24 @@ impl From<ParseError> for MyError {
     }
 }
 
-
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// destination folder
-    #[arg(short = 'f', long)]
-    folder_path: String,
+    /// destination folder (with -f/--folder-path)
+    #[arg(short = 'f', long, value_name = "FOLDER")]
+    folder_path: Option<String>,
 
-    /// Size of folder
-    #[arg(short= 's', long)]
-    size: String,
+    /// destination folder (positional argument)
+    #[arg(index = 1)]
+    positional_folder_path: Option<String>,
+
+    /// Size of folder (with -s/--size)
+    #[arg(short = 's', long, value_name = "SIZE")]
+    size: Option<String>,
+
+    /// Size of folder (positional argument)
+    #[arg(index = 2)]
+    positional_size: Option<String>,
 
     /// Depth of recurse
     #[arg(short, long, default_value_t = 1)]
@@ -68,9 +74,16 @@ struct Args {
 
 fn main() -> Result<(), BigError> {
     let args = Args::parse();
-    let folder_path = Path::new(&args.folder_path).to_path_buf();
-    fs::create_dir_all(&folder_path)?;
-    let total_size = parse_size(&args.size)?.as_u64();
+    let folder_path = args
+        .folder_path
+        .or(args.positional_folder_path)
+        .ok_or_else(|| BigError::from("Folder path must be specified"))?;
+    let folder_path = Path::new(&folder_path).to_path_buf();
+    let size = args
+        .size
+        .or(args.positional_size)
+        .ok_or_else(|| BigError::from("Size be specified"))?;
+    let total_size = parse_size(&size)?.as_u64();
     let depth = args.d;
     let min_file_size = parse_size(&args.min)?.as_u64();
     let mut max_file_size = parse_size(&args.max)?.as_u64();
@@ -90,6 +103,8 @@ fn main() -> Result<(), BigError> {
             .progress_chars("#>-"),
     );
 
+
+    fs::create_dir_all(&folder_path)?;
     while current_size < total_size {
         let file_size = rng.gen_range(min_file_size..=max_file_size);
         let file_name = format!("file_{}.txt", file_number);
@@ -131,7 +146,6 @@ fn create_nested_path(base_path: &Path, depth: u64, rng: &mut impl Rng) -> PathB
     }
     current_path
 }
-
 
 fn parse_size(s: &str) -> Result<Byte, MyError> {
     Byte::parse_str(s, true).map_err(MyError::from)
